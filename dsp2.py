f0f59542d6a156231ee75e8f687e6a1283f2a48a
loopag1 <161476268+loopag1@users.noreply.github.com>
2024-02-27 16:21:35 +0200

    Add files via upload

---
+++ dsp2.py
@@ -0,0 +1,4 @@
+import random
+choicer = ["r", "a", "n", "d", "o","m"]
+choic = random.choice(choicer)
+print(choic)
---
+++ pds1.py
@@ -0,0 +1,9 @@
+import random
+list = ["dad", "son", "mom", "lilsister"]
+ind = input("random: ")
+if ind == input("random: "):
+    intf = random.randint(1, 100)
+    print(intf)
+    print(type(intf))
+fam = random.choice(list)
+print(fam)
